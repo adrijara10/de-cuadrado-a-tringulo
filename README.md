678bb965ec288c7146a8f5b6cf0d04d92596d94b
adrijara10 <84816010+adrijara10@users.noreply.github.com>
2021-09-12 17:34:33 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Abra esta pagina en [https://adrijara10.github.io/de-cuadrado-a-tringulo/](https://adrijara10.github.io/de-cuadrado-a-tringulo/)
+
+## Usar como extensión
+
+Este repositorio puede ser añadido como una **extensión** en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **New Project**
+* haga clic en **Extensiones** en el menú del engranaje
+* buscar **https://github.com/adrijara10/de-cuadrado-a-tringulo** e importar
+
+## Edita este proyecto ![Insignia de estado de compilación](https://github.com/adrijara10/de-cuadrado-a-tringulo/workflows/MakeCode/badge.svg)
+
+Para editar este repositorio en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **Import** luego haga clic en **Import URL**
+* pegue **https://github.com/adrijara10/de-cuadrado-a-tringulo** y haga clic en importar
+
+## Vista previa de bloques
+
+Esta imagen muestra el código de los bloques de la última confirmación en master.
+Esta imagen puede tardar unos minutos en actualizarse.
+
+![Una vista renderizada de los bloques](https://github.com/adrijara10/de-cuadrado-a-tringulo/raw/master/.github/makecode/blocks.png)
+
+#### Metadatos (utilizados para búsqueda, renderizado)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,103 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . # # #
+        . # . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . # . #
+        . # . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        . # . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # . #
+        . # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
+})
